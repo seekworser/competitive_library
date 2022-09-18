b76612997a7c0a14952fc6b7f35ab5e2af59d778
seekworser <seekworser1963@gmail.com>
2022-09-18 09:43:17 +0900

    fix bit.hpp bug

--- competitive/math/bit.hpp
+++ competitive/math/bit.hpp
@@ -7,10 +7,10 @@
 namespace bit{
     bool lb(ll x) { return (x & 1); }
     bool get(ll x, int n) { return ((x >> n) & 1); }
-    ll update(ll x, int n, bool b) { return (x & ~(1 << n)) + (b << n); }
+    ll update(ll x, int n, bool b) { return (x & ~(1LL << n)) + (b << n); }
     ll lsb(ll x) { return -x & x; }
     int digit(ll x) { int rev=0; while(x) { rev++; x >>= 1; } return rev; }
-    ll msb(ll x) { if (x==0) return 0; return 1 << (digit(x) - 1); }
+    ll msb(ll x) { if (x==0) return 0; return 1LL << (digit(x) - 1); }
     int lsbd(ll x) {return digit(lsb(x)); }
     int count(ll x) { int rev=0; while(x) { if (lb(x)) rev++; x >>= 1; } return rev; }
     string to_string(ll x, bool rev=true) { stringstream ss; while (x) { ss << lb(x); x >>= 1;} string s(ss.str()); if (rev) reverse(all(s)); return s; }
