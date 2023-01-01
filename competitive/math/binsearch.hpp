f7b337eb215d997b71a7850f7d154f490b5ddcd8
seekworser <seekworser1963@gmail.com>
2023-01-01 09:41:02 +0900

    divide ternary_search to for double and for integer

--- competitive/math/binsearch.hpp
+++ competitive/math/binsearch.hpp
@@ -24,12 +24,29 @@ template <class F> double binsearch_double(double s, double e, F le, double eps=
     return cmin;
 }
 //s~eで高々1つしか極値を持たないfについて極値を与える点を返す
-template <class T, class F> T ternary_search_double(T s, T e, F f, double eps=1e-10) {
+template <class T, class F> T ternary_search(T s, T e, F f, double eps=1e-10) {
     T cmin = s;
     T cmax = e;
+    while (cmax - cmin > (T)3) {
+        T p1 = cmin + max<T>(1, (cmax - cmin) / 3);
+        T p2 = cmax - max<T>(1, (cmax - cmin) / 3);
+        if (f(p1) > f(p2)) cmin = p1;
+        else cmax = p2;
+    }
+    T rev = cmin;
+    auto val = f(cmin);
+    rep(i, 3) {
+        if (chmin<double>(val, f(cmin+i))) rev = cmin + i;
+    }
+    return rev;
+}
+//s~eで高々1つしか極値を持たないfについて極値を与える点を返す
+template <class F> double ternary_search_double(double s, double e, F f, double eps=1e-10) {
+    double cmin = s;
+    double  cmax = e;
     while (abs(cmax - cmin) > eps) {
-        T p1 = cmin + (cmax - cmin) / 3;
-        T p2 = cmax - (cmax - cmin) / 3;
+        double p1 = cmin + (cmax - cmin) / 3;
+        double p2 = cmax - (cmax - cmin) / 3;
         if (f(p1) > f(p2)) cmin = p1;
         else cmax = p2;
     }
