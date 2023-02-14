1cc07e1c7bac83b8da931875a0fa1b304b556a05
seekworser <seekworser1963@gmail.com>
2023-02-14 16:55:08 +0900

    add is for modint

--- competitive/math/modint.hpp
+++ competitive/math/modint.hpp
@@ -3,18 +3,18 @@
 #include <competitive/std/std.hpp>
 #include <atcoder/modint.hpp>
 // istream in not working...
-// inline std::istream& operator>>(std::istream& is, atcoder::modint x);
-// inline std::istream& operator>>(std::istream& is, atcoder::modint998244353 x);
-// inline std::istream& operator>>(std::istream& is, atcoder::modint1000000007 x);
-inline std::ostream& operator<<(std::ostream& os, const atcoder::modint x);
-inline std::ostream& operator<<(std::ostream& os, const atcoder::modint998244353 x);
-inline std::ostream& operator<<(std::ostream& os, const atcoder::modint1000000007 x);
-// inline std::istream& operator>>(std::istream& is, atcoder::modint x) {ll a; is >> a; x -= x.val(); x += a; return is; }
-// inline std::istream& operator>>(std::istream& is, atcoder::modint998244353 x) {ll a; is >> a; x -= x.val(); x += a; return is; }
-// inline std::istream& operator>>(std::istream& is, atcoder::modint1000000007 x) {ll a; is >> a; x -= x.val(); x += a; return is; }
-inline std::ostream& operator<<(std::ostream& os, const atcoder::modint x) { os << x.val(); return os; }
-inline std::ostream& operator<<(std::ostream& os, const atcoder::modint998244353 x) { os << x.val(); return os; }
-inline std::ostream& operator<<(std::ostream& os, const atcoder::modint1000000007 x) { os << x.val(); return os; }
+inline std::istream& operator>>(std::istream& is, atcoder::modint& x);
+inline std::istream& operator>>(std::istream& is, atcoder::modint998244353& x);
+inline std::istream& operator>>(std::istream& is, atcoder::modint1000000007& x);
+inline std::ostream& operator<<(std::ostream& os, const atcoder::modint& x);
+inline std::ostream& operator<<(std::ostream& os, const atcoder::modint998244353& x);
+inline std::ostream& operator<<(std::ostream& os, const atcoder::modint1000000007& x);
+inline std::istream& operator>>(std::istream& is, atcoder::modint& x) {ll a; is >> a; x = a; return is; }
+inline std::istream& operator>>(std::istream& is, atcoder::modint998244353& x) {ll a; is >> a; x = a; return is; }
+inline std::istream& operator>>(std::istream& is, atcoder::modint1000000007& x) {ll a; is >> a; x = a; return is; }
+inline std::ostream& operator<<(std::ostream& os, const atcoder::modint& x) { os << x.val(); return os; }
+inline std::ostream& operator<<(std::ostream& os, const atcoder::modint998244353& x) { os << x.val(); return os; }
+inline std::ostream& operator<<(std::ostream& os, const atcoder::modint1000000007& x) { os << x.val(); return os; }
 using modint998244353 = atcoder::modint998244353;
 using modint1000000007 = atcoder::modint1000000007;
 using modint = atcoder::modint;
