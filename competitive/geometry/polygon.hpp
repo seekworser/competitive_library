58cee2eec1bace5f3780e5c68a84f0ffa0179859
seekworser <seekworser1963@gmail.com>
2023-02-15 22:44:03 +0900

    add convex full

--- competitive/geometry/polygon.hpp
+++ competitive/geometry/polygon.hpp
@@ -24,4 +24,46 @@ template<typename T> bool is_convex(const Polygon<T> &p, bool strict=true) {
     }
     return rev;
 }
+template<typename T> Polygon<T> convex_full(const vector<Point<T>> &p, bool strict=true) {
+    int n = sz(p);
+    if (n < 3) return Polygon<T>(p);
+    vector<Point<T>> sorted(p);
+    sort(all(sorted));
+    vector<Point<T>> rev;
+    rev.push_back(sorted[0]);
+    rev.push_back(sorted[1]);
+    repi(i, 2, n) {
+        int m = sz(rev);
+        if (strict) {
+            while (m >= 2 && ccw(rev[m-2], rev[m-1], sorted[i]) != CLOCKWISE) {
+                rev.pop_back();
+                --m;
+            }
+        } else {
+            while (m >= 2 && ccw(rev[m-2], rev[m-1], sorted[i]) == COUNTER_CLOCKWISE) {
+                rev.pop_back();
+                --m;
+            }
+        }
+        rev.push_back(sorted[i]);
+    }
+    int lower_size = sz(rev);
+    repir(i, n-2, -1, -1) {
+        int m = sz(rev);
+        if (strict) {
+            while (m > lower_size && ccw(rev[m-2], rev[m-1], sorted[i]) != CLOCKWISE) {
+                rev.pop_back();
+                --m;
+            }
+        } else {
+            while (m > lower_size && ccw(rev[m-2], rev[m-1], sorted[i]) == COUNTER_CLOCKWISE) {
+                rev.pop_back();
+                --m;
+            }
+        }
+        rev.push_back(sorted[i]);
+    }
+    rev.erase(rev.begin());
+    return Polygon<T>(rev);
+}
 #endif // COMPETITIVE_GEOMERY_POLYGON_HPP
---
+++ online_test/AOJ/CGL_4_A.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/3/CGL_4_A"
+#include <competitive/std/std.hpp>
+#include <competitive/geometry/polygon.hpp>
+#include <competitive/std/io.hpp>
+
+int main() {
+    int n;
+    cin >> n;
+    using P = Point<ll>;
+    vector<P> points;
+    rep(i, n) {
+        ll x, y;
+        cin >> x >> y;
+        points.emplace_back(x, y);
+    }
+    Polygon<ll> p = convex_full(points, false);
+    cout << sz(p.vertex) << endl;
+    int pos = 0;
+    pll cur = {p.vertex[0].y, p.vertex[0].x};
+    rep(i, sz(p.vertex)) {
+        if (chmin(cur, pll(p.vertex[i].y, p.vertex[i].x))) pos = i;
+    }
+    rep(i, sz(p.vertex)) {
+        cout << p.vertex[pos] << endl;
+        pos++;
+        pos %= sz(p.vertex);
+    }
+}
