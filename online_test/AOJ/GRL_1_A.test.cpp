6eb91ab97b87e3046fbd686bfb68c1a53b015eb5
seekworser <seekworser1963@gmail.com>
2023-02-17 13:18:30 +0900

    add dijkstra test

---
+++ online_test/AOJ/GRL_1_A.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A"
+#include <competitive/std/std.hpp>
+#include <competitive/graph/dijkstra.hpp>
+#include <competitive/std/io.hpp>
+
+int main() {
+    int n,m,r;
+    cin >> n >> m >> r;
+    vi from(m), to(m); vl cost(m);
+    rep(i, m) cin >> from[i] >> to[i] >> cost[i];
+    Graph g(n, from, to, cost);
+    vi prev; vl ans;
+    dijkstra(g, r, ans, prev, INFL);
+    rep(i, n) {
+        if (ans[i] == INFL) cout << "INF\n";
+        else cout << ans[i] << "\n";
+    }
+}
