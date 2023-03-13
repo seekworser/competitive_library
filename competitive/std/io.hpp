eee231a478fff430d1daf5f73d19d2f39e092490
seekworser <seekworser1963@gmail.com>
2023-03-14 00:41:13 +0900

    add read/input macros

--- competitive/std/io.hpp
+++ competitive/std/io.hpp
@@ -31,15 +31,33 @@ template <typename T1, typename... T2> void print_sep_end(string sep, string end
     cout << val << sep;
     print_sep_end(sep, end, remain...);
 };
-template <typename... T> void print(const T&...args) { print_sep_end(" ", "\n", args...); };
+template <typename... T> void print(const T &...args) { print_sep_end(" ", "\n", args...); };
 template <typename... T> void flush() { cout << flush; };
-template <typename... T> void print_and_flush(const T&...args) { print(args...); flush(); };
+template <typename... T> void print_and_flush(const T &...args) { print(args...); flush(); };
 #define debug(...) debug_func(0, #__VA_ARGS__, __VA_ARGS__) // debug print
+template <typename T> void read(T &a) { cin >> a; };
+template <typename T1, typename... T2> void read(T1&a, T2 &...b) { cin >> a; read(b...); };
+#define input(type, ...) type __VA_ARGS__; read(__VA_ARGS__);
+#define vec(type, name, ...) vector<type> name(__VA_ARGS__)  // vec(type, name, n, init(optiobal))
+#define vvec(type, name, h, ...) vector name(h, vector<type>(__VA_ARGS__))
+#define vvvec(type, name, h, w, ...) vector name(h, vector(w, vector<type>(__VA_ARGS__)))
+#define vvvvec(type, name, h1, h2, h3, ...) vector name(h1, vector(h2, vector(h3, vector<type>(__VA_ARGS__))))
+#define vvvvvec(type, name, h1, h2, h3, h4, ...) vector name(h1, vector(h2, vector(h3, vector(h4, vector<type>(__VA_ARGS__)))))
+#define input_vec(type, name, ...) vec(type, name, __VA_ARGS__); read(name);
+#define input_vvec(type, name, h, ...) vvec(type, name, h, __VA_ARGS__); read(name);
+#define input_vvvec(type, name, h, w, ...) vvvec(type, name, h, w, __VA_ARGS__); read(name);
+#define input_vvvvec(type, name, h1, h2, h3, ...) vvvvec(type, name, h1, h2, h3, __VA_ARGS__); read(name);
+#define input_vvvvvec(type, name, h1, h2, h3, h4, ...) vvvvvec(type, name, h1, h2, h3, h4, __VA_ARGS__); read(name);
 #ifdef LOCAL_TEST
 template <typename T>
 void debug_func(int i, T name) { (void)i; (void)name; cerr << endl; }
 template <typename T1, typename T2, typename... T3> void debug_func(int i, const T1 &name, const T2 &a, const T3 &...b) {
-    for ( ; name[i] != ',' && name[i] != '\0'; i++ ) cerr << name[i];
+    int scope = 0;
+    for ( ; (scope != 0 || name[i] != ',') && name[i] != '\0'; i++ ) {
+        cerr << name[i];
+        if (name[i] == '(' || name[i] == '{') scope++;
+        if (name[i] == ')' || name[i] == '}') scope--;
+    }
     cerr << ":" << a << " ";
     debug_func(i + 1, name, b...);
 }
