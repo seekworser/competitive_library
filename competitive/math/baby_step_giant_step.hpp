ce5cffd27733460d3778a3a1d8e75b4f2a910b22
seekworser <seekworser1963@gmail.com>
2023-03-30 02:13:01 +0900

    add baby-step-giand-step

--- competitive/math/baby_step_giant_step.hpp
+++ competitive/math/baby_step_giant_step.hpp
@@ -1,6 +1,33 @@
 #pragma once
 #include "competitive/std/std.hpp"
+#include "competitive/math/euler_phi.hpp"
+// Find k where x^k = y (mod p)
+ll baby_step_giant_step(ll x, ll y, ll p) {
+    assert(gcd(x, p) == 1);
+    ll m = sqrtll(p);
+    unordered_map<ll, int> xpow_pos;
+    ll r = 1;
+    int cnt = 0;
+    auto insert_xp = [&](){
+        if (!xpow_pos.count(r)) xpow_pos[r] = cnt++;
+        r = (r * x);
+        r %= p;
+    };
+    rep(i, m) { insert_xp(); }
+    while (gcd(r, p) != 1) {
+        m++;
+        insert_xp();
+    }
+    r = powm(r, euler_phi(p)-1, p);
+    ll rp = y % p;
+    rep(i, m+1) {
+        if (xpow_pos.count(rp)) return i * m + xpow_pos[rp];
+        rp *= r;
+        rp %= p;
+    }
+    return -1;
+}
 /**
- * @brief baby_step_giant_step
+ * @brief Baby Step Giant Step
  * @docs docs/math/baby_step_giant_step.md
  */
