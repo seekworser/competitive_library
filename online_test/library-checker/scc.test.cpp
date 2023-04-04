e956742fcc3a4a13b60411ec5afa76c04817335c
seekworser <seekworser1963@gmail.com>
2023-04-04 11:02:47 +0900

    add scc verify (yosupo judge)

---
+++ online_test/library-checker/scc.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/longest_increasing_subsequence"
+#include "competitive/std/std.hpp"
+#include "competitive/graph/scc.hpp"
+#include "competitive/std/io.hpp"
+int main() {
+    ll n,m;
+    input(n, m);
+    vi from(m), to(m), cost(m, 1);
+    rep(i, m) input(from[i], to[i]);
+    Graph g(n, from, to, cost);
+    auto scc = scc_decompose(g);
+    print(sz(scc));
+    repe(e, scc) {
+        print_sep_end("", " ", sz(e));
+        print(e);
+    }
+}
