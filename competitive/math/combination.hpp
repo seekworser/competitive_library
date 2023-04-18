9fc6d55c2455ecac35c556e89274d9aa8b51c059
seekworser <seekworser1963@gmail.com>
2023-04-19 01:05:56 +0900

    change modint to use mint

--- competitive/math/combination.hpp
+++ competitive/math/combination.hpp
@@ -1,24 +1,24 @@
 #pragma once
 #include "competitive/std/std.hpp"
-struct Combination {
-    ll pm;
-    vl fact, fact_inv;
-    Combination(int n = 1000003, ll p = 998244353) : fact(n), fact_inv(n){
-        vl inv(n);
-        pm = p;
+#include "competitive/math/modint.hpp"
+template<typename mint> struct Combination {
+    vector<mint> fact, fact_inv;
+    Combination(int nmax) : fact(nmax+1), fact_inv(nmax+1) {
+        int p = mint::mod();
+        vector<mint> inv(nmax+1);
         fact[0] = fact[1] = 1;
         fact_inv[0] = fact_inv[1] = 1;
         inv[0] = 0;
         inv[1] = 1;
-        for (int i = 2; i < n; i++) {
-            fact[i] = fact[i - 1] * i % p;
-            inv[i] = p - inv[p % i] * (p / i) % p;
-            fact_inv[i] = fact_inv[i - 1] * inv[i] % p;
+        for (int i = 2; i < nmax+1; i++) {
+            fact[i] = fact[i - 1] * i;
+            inv[i] = p - inv[p % i] * (p / i);
+            fact_inv[i] = fact_inv[i - 1] * inv[i];
         }
     }
-    ll operator()(int n, int r) {
+    mint operator()(int n, int r) {
         if (r < 0 || n < r) return 0;
-        return fact[n] * (fact_inv[r] * fact_inv[n - r] % pm) % pm;
+        return fact[n] / fact_inv[r] / fact_inv[n - r];
     }
 };
 /**
